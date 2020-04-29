26caca70438eeb4458ccd41e7a93640bf78aa4f3
shenry45 <shawn45henry@gmail.com>
2020-04-28 21:12:52 -0700

    CPU constructor, run(), ram_read(), ram_write(), ALU op PRN & LDI

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -4,18 +4,21 @@ import sys
 
 class CPU:
     """Main CPU class."""
-
     def __init__(self):
         """Construct a new CPU."""
-        self.reg = [0] * 8,
-        self.ram = [],
-        self.pc = '',
+        self.reg = [0] * 8
+        self.ram = [0] * 256
         self.ir = {
+            'PC': 00,
+            'IR': '',
             'MAR': '',
             'MDR': ''
-        },
-        self.fl = ''
-
+        }
+        self.fl = {
+            'L': 0,
+            'G': 0,
+            'E': 0
+        }
 
     def load(self):
         """Load a program into memory."""
@@ -26,16 +29,16 @@ class CPU:
 
         program = [
             # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
+            0b10000010, # LDI
+            0b00000000, # REG 0 identifier
+            0b00001000, # 8 value
+            0b01000111, # PRN
+            0b00000000, # REG 0 identifier
             0b00000001, # HLT
         ]
 
         for instruction in program:
-            setattr(self.ram[address], instruction)
+            self.ram[address] = instruction
             address += 1
 
 
@@ -46,7 +49,117 @@ class CPU:
             reg_a = getattr(self.ram[reg_a])
             reg_b = getattr(self.reg[reg_b])
             setattr(self.reg[reg_a], reg_a + reg_b)
-        #elif op == "SUB": etc
+        elif op == "AND":
+            reg_a = getattr(self.ram[reg_a])
+            reg_b = getattr(self.reg[reg_b])
+            setattr(self.reg[reg_a], reg_a & reg_b)
+        elif op == "CALL":
+            # TODO: Push address to top of stack
+            # reverse counter after?
+            pass
+        elif op == "CMP":
+            if reg_a == reg_b:
+                setattr(self.fl['E'], 1)
+            else:
+                setattr(self.fl['E'], 0)
+
+            if reg_a < reg_b:
+                setattr(self.fl['L'], 1)
+            else:
+                setattr(self.fl['L'], 0)
+            
+            if reg_a > reg_b:
+                setattr(self.fl['G'], 1)
+            else:
+                setattr(self.fl['G'], 0)
+        elif op == 'DEC':
+            setattr(self.reg[reg_a], reg_a - 1)
+        elif op == 'DIV':
+            reg_a = getattr(self.ram[reg_a])
+            reg_b = getattr(self.reg[reg_b])
+            if reg_b == 0:
+                # TODO Halt
+                print('Error on operation: DIV')
+        elif op == 0b00000001:
+            '''HLT'''
+            exit()
+        elif op == 'INC':
+            setattr(self.reg[reg_a], reg_a + 1)
+        elif op == 'INT':
+            setattr(self.reg[7], reg_a)
+        elif op == 'IRET':
+            pass
+        elif op == 'JEQ':
+            pass
+        elif op == 'JGE':
+            pass
+        elif op == 'JGT':
+            pass
+        elif op == 'JLE':
+            pass
+        elif op == 'JLT':
+            pass
+        elif op == 'JMP':
+            pass
+        elif op == 'JNE':
+            pass
+        elif op == 'LD':
+            pass
+        elif op == 0b10000010:
+            '''LDI'''
+            # get reg address to write to
+            reg_addr = self.ir['PC'] + 1
+            reg_val = self.ram[reg_addr]
+            # get int addr for reg write
+            int_addr = self.ir['PC'] + 2
+            integer_val = self.ram[int_addr]
+            # set reg addr to int value
+            self.ram_write(reg_val, integer_val)
+            
+            self.ir['PC'] += 3
+        elif op == 'MOD':
+            pass
+        elif op == 'MUL':
+            pass
+        elif op == 'NOP':
+            pass
+        elif op == 'NOT':
+            pass
+        elif op == 'OR':
+            pass
+        elif op == 'POP':
+            pass
+        elif op == 'PRA':
+            pass
+        elif op == 0b01000111:
+            '''PRN'''
+            print(self.reg[reg_a])
+            self.ir['PC'] += 2
+
+        elif op == 'PUSH':
+            # change SP
+            self.reg[7] -= 1
+            # copy value from register into memory
+            reg_num = self.ram[self.ir['PC'] + 1]
+            value = self.ram[reg_num + 1]
+
+            address = self.reg[7]
+            self.ram[address] = value
+
+            pc += 2
+            
+        elif op == 'RET':
+            pass
+        elif op == 'SHL':
+            setattr(self.reg[reg_a], reg_a << reg_b)
+        elif op == 'SHR':
+            setattr(self.reg[reg_a], reg_a >> reg_b)
+        elif op == 'ST':
+            setattr(self.reg[reg_b], reg_a)
+        elif op == 'SUB':
+            setattr(self.reg[reg_a], reg_a - reg_b)
+        elif op == 'XOR':
+            setattr(self.reg[reg_a], reg_a ^ reg_b)
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -70,12 +183,23 @@ class CPU:
 
         print()
 
-    def ram_read(self, mar):
-        return getattr(self.ram[mar])
-
-    def ram_write(self, addr, mar, mdr):
-        setattr(self.ram[mar], mdr)
+    def ram_read(self, reg):
+        return self.ram[reg]
+    def ram_write(self, reg, val):
+        self.reg[reg] = val
 
     def run(self):
         """Run the CPU."""
-        pass
+        # init stack pointer at F4
+        self.reg[7] = 0xF4
+        # init program counter at 00
+        self.ir['PC'] = 0x00
+
+        # run commands until
+        while self.ram[self.ir['PC']] != 0:
+            command_addr = self.ir['PC']
+            reg_a_val = self.ram[command_addr + 1]
+            reg_b_val = self.ram[command_addr + 2]
+            command_val = self.ram[command_addr]
+            # run command
+            self.alu(command_val, reg_a_val, reg_b_val)
